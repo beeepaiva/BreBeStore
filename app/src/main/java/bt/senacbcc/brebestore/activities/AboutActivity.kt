ec09139096c3e820cfd2d0c66a642ffa94b53aa0
Beatriz Paiva <beeepaiva@gmail.com>
2020-11-16 16:47:58 -0300

    About activity

--- app/src/main/java/bt/senacbcc/brebestore/activities/AboutActivity.kt
+++ app/src/main/java/bt/senacbcc/brebestore/activities/AboutActivity.kt
@@ -1,12 +1,34 @@
 package bt.senacbcc.brebestore.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import bt.senacbcc.brebestore.R
+import kotlinx.android.synthetic.main.activity_about.*
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        imgSenac.setOnClickListener{
+            val i = Intent(Intent.ACTION_VIEW,
+            Uri.parse("https://www.sp.senac.br/"))
+            startActivity(i)
+        }
+
+        imgGitHub.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW,
+                Uri.parse("https://github.com/beeepaiva/BreBeStore"))
+            startActivity(i)
+        }
+        imgEmail.setOnClickListener{
+            val intent =
+                Intent(Intent.ACTION_VIEW,
+                    Uri.parse("mailto:" + "brebebcc@gmail.com")
+                )
+            startActivity(intent)
+        }
     }
 }
